4dd4468727f1a3772a6bc807d15941498867bedb
Malin <Malin@Malin-HP>
2017-10-04 11:51:11 +0200

    tetttt

--- Gittest/src/Test.java
+++ Gittest/src/Test.java
@@ -3,7 +3,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("this is a teswt2");
+		System.out.println("this is a teswt244");
 
 	}
 
